84c0d0b887813365744ea59bfb2f5d0d906a864b
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-11-15 16:37:05 +0100

    Added Menu Route
    * Added menu route
    * Added buttons to confirmation & booking

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Menu.css";
+import navicon from "../assets/navicon.svg";
 
 const Menu: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Menu</h1>
-      {}
+    <div className="menu-page">
+      <img
+        src={navicon}
+        alt="Close Menu"
+        className="navicon"
+        onClick={() => navigate(-1)}
+      />
+      <button className="menu-button" onClick={() => navigate("/booking")}>
+        Booking
+      </button>
+      <button className="menu-button" onClick={() => navigate("/confirmation")}>
+        Confirmation
+      </button>
     </div>
   );
 };
